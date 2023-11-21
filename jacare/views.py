e3799f341f48ef4cbe0e29a08a929aca899e43de
Kai Eugene <kai.yougene@gmail.com>
2023-11-21 14:41:25 +0900

    feat: implement register endpoint

--- jacare/views.py
+++ jacare/views.py
@@ -1,12 +1,12 @@
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 from .models import Customer
 from firebase_admin import auth
 
 
 @csrf_exempt
 def login_user(request):
-    # Obter UID do usuário a partir do cabeçalho Authorization
     uid = request.headers.get("Authorization", '').split('Bearer ')[-1]
     print(user)
     
@@ -18,28 +18,18 @@ def login_user(request):
     else: 
         return JsonResponse({'Error': 'Please register before loggin in'}, status=401)
 
-
-# @csrf_exempt
-# def register_user(request):
-#     uid = request.headers.get("Authorization", '').split('Bearer ')[-1]
-    
-    
-
-
-
+@api_view(['POST'])
 @csrf_exempt
-def create_customer_review(request):
-    # Adicione aqui a lógica para criar uma avaliação de cliente
-    # Certifique-se de que o usuário esteja autenticado antes de criar a revisão
+def register_user(request):
+    uid = request.headers.get("Authorization", '').split('Bearer ')[-1]
+    
+    user_exists = Customer.objects.filter(customer_uid=uid).exists()
 
-    # Exemplo simples de resposta JSON
-    return JsonResponse({'message': 'Avaliação do cliente criada com sucesso!'})
+    if user_exists:
+        return JsonResponse({'error': 'User already registered'}, status=400)
+    else:
+        new_user = Customer(customer_uid=uid)
+        new_user.save()
 
-# Adicione mais views conforme necessário para outras operações no seu banco de dados
-
-# Exemplo de uma view que lista todos os clientes
-@csrf_exempt
-def list_customers(request):
-    customers = Customer.objects.all()
-    customer_list = [{'customer_uid': customer.customer_uid, 'first_name': customer.first_name, 'last_name': customer.last_name} for customer in customers]
-    return JsonResponse({'customers': customer_list})
+        return JsonResponse({'success': 'User registered successfully'}, status=201)
+    
