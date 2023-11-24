d016410a96c75720eb7c5b10ea2832801ff124a6
Kai Eugene <kai.yougene@gmail.com>
2023-11-24 19:10:43 +0900

    feat: implement openNow filter

--- jacare/views.py
+++ jacare/views.py
@@ -8,7 +8,7 @@ import requests
 import os 
 api_key = os.environ.get("API_KEY")
 
-def filter_by_price(places, userPrice):
+def filter_data(places, user_price, open_now=False):
     google_price_level = {
         "PRICE_LEVEL_FREE": 0,
         "PRICE_LEVEL_INEXPENSIVE": 1,
@@ -17,55 +17,62 @@ def filter_by_price(places, userPrice):
         "PRICE_LEVEL_VERY_EXPENSIVE": 4,
     }
 
-    filtered_places = [
-        place for place in places
-        if google_price_level.get(place.get('priceLevel'), 0) <= userPrice
-    ]
+    if open_now is True:
+        result = [
+            place for place in places
+            if google_price_level.get(place.get("priceLevel"), 0) <= user_price and
+                (place.get("currentOpeningHours", {}).get("openNow", False) if "currentOpeningHours" in place else False)
+        ]
+    else: 
+        result = [
+            place for place in places
+            if google_price_level.get(place.get("priceLevel"), 0) <= user_price
+        ]
 
-    return filtered_places
+    return result
 
 @csrf_exempt
 def login_user(request):
-    uid = request.headers.get("Authorization", '').split('Bearer ')[-1]
+    uid = request.headers.get("Authorization", "").split('Bearer ')[-1]
     print(uid)  
 
     user = Customer.objects.filter(customer_uid=uid).exists()
     print(user)
     
     if user:
-        return JsonResponse({'success': 'Logged in'}, status=200)
+        return JsonResponse({"success": "Logged in"}, status=200)
     else: 
-        return JsonResponse({'Error': 'Please register before logging in'}, status=401)
+        return JsonResponse({"Error": "Please register before logging in"}, status=401)
 
 @api_view(['POST'])
 @csrf_exempt
 def register_user(request):
     body_unicode = request.body.decode('utf-8')
     body = json.loads(body_unicode)
-    uid = body['uid']
+    uid = body["uid"]
     print(uid)
     
     
     user_exists = Customer.objects.filter(customer_uid=uid).exists()
 
     if user_exists:
-        return JsonResponse({'error': 'User already registered'}, status=400)
+        return JsonResponse({"error": "User already registered"}, status=400)
     else:
         new_user = Customer(customer_uid=uid)
         new_user.save()
 
-        return JsonResponse({'success': 'User registered successfully'}, status=201)
+        return JsonResponse({"success": "User registered successfully"}, status=201)
     
 @api_view(['POST'])
 def query_restaraurant(request):
 
     body = request.data
-    cuisine_type = body.get('cuisineType', None)
-    location = body.get('location', None)
-    price = body.get('price', None)
-    distance = body.get('distanceToTravel', None)
-    max_result_count = body.get('amountOfOptions', None)
-    openNow = body.get('openNow', None)
+    cuisine_type = body.get("cuisineType", None)
+    location = body.get("location", None)
+    price = body.get("price", None)
+    distance = body.get("distanceToTravel", None)
+    max_result_count = body.get("amountOfOptions", None)
+    openNow = body.get("openNow", None)
 
     location_restriction = { 
             "circle": {
@@ -86,14 +93,14 @@ def query_restaraurant(request):
     headers = {
         "Content-Type": "application/json",
         "X-Goog-Api-Key": api_key,
-        "X-Goog-FieldMask":"places.displayName,places.priceLevel"
+        "X-Goog-FieldMask":"places.location,places.displayName,places.priceLevel,places.currentOpeningHours"
     }
 
     response = requests.post("https://places.googleapis.com/v1/places:searchNearby", json=data, headers=headers)
 
     if response.status_code == 200:
         data = response.json()
-        filtered_results = filter_by_price(data.get('places', []), price)
+        filtered_results = filter_data(data.get('places', []), price, openNow)
         return JsonResponse({"result": filtered_results}, status=200)
     else:
-        return JsonResponse({'error': 'Failed to fetch data from Google Places API'}, status=response.status_code)
+        return JsonResponse({"error": "Failed to fetch data from Google Places API"}, status=response.status_code)
