0d77e3265deec446e5ea6395d4e6389c57072e87
Kai Eugene <kai.yougene@gmail.com>
2023-11-24 16:41:40 +0900

    feat: implement search endpoint

--- jacare/views.py
+++ jacare/views.py
@@ -4,7 +4,10 @@ from rest_framework.decorators import api_view
 from .models import Customer
 from firebase_admin import auth
 import json
+import requests
+import os 
 
+api_key = os.environ.get("API_KEY")
 
 @csrf_exempt
 def login_user(request):
@@ -38,3 +41,45 @@ def register_user(request):
 
         return JsonResponse({'success': 'User registered successfully'}, status=201)
     
+@api_view(['POST'])
+def query_restaraurant(request):
+
+    body = request.data
+    cuisineType = body.get('cuisineType', None)
+    location = body.get('location', None)
+    price = body.get('price', None)
+    distance = body.get('distanceToTravel', None)
+    maxResultCount = body.get('amountOfOptions', None)
+    openNow = body.get('openNow', None)
+
+    locationRestriction = { 
+            "circle": {
+                "center": {
+                    "latitude": location["latitude"],
+                    "longitude": location["longitude"],
+                },
+            "radius": distance
+        }   
+    }
+
+    data = {
+        "includedTypes": [cuisineType],
+        "maxResultCount": maxResultCount,
+        "locationRestriction": locationRestriction,
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": api_key,
+        "X-Goog-FieldMask":"places.displayName"
+    }
+
+    response = requests.post("https://places.googleapis.com/v1/places:searchNearby", json=data, headers=headers)
+
+    print(response.json())
+    if response.status_code == 200:
+        data = response.json()
+        return JsonResponse({"result": data["places"]}, status=200)
+    else:
+        return JsonResponse({'error': 'Failed to fetch data from Google Places API'}, status=response.status_code)
+    # return JsonResponse({"success": params}, status=200)
--- setup/urls.py
+++ setup/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from jacare.views import login_user, register_user
+from jacare.views import login_user, register_user, query_restaraurant
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login_user),
-    path('register/', register_user)
+    path('register/', register_user),
+    path('search/', query_restaraurant)
 ]
