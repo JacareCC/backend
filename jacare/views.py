03fe3b00edbfa12801a1e5cd7f0a24b9800d1602
Will Brammer <williamedwardbrammer@gmail.com>
2023-11-27 18:38:57 +0900

    feat: made it so that weights control the random result list

--- jacare/views.py
+++ jacare/views.py
@@ -7,6 +7,8 @@ from firebase_admin import auth
 import json
 import requests
 import os 
+import random
+import numpy as np
 api_key = os.environ.get("API_KEY")
 
 #This is a helper function to filter returned restaraunts from google
@@ -74,8 +76,8 @@ def query_restaraurant(request):
     location = body.get("location", None)
     price = body.get("price", None)
     distance = body.get("distanceToTravel", None)
-    max_result_count = body.get("amountOfOptions", None)
     openNow = body.get("openNow", None)
+    max_result_count = body.get("amountOfOptions", None)
 
     location_restriction = { 
             "circle": {
@@ -89,7 +91,7 @@ def query_restaraurant(request):
 
     data = {
         "includedTypes": cuisine_options,
-        "maxResultCount": max_result_count,
+        # "maxResultCount": max_result_count,
         "locationRestriction": location_restriction,
     }
 
@@ -101,12 +103,29 @@ def query_restaraurant(request):
 
     response = requests.post("https://places.googleapis.com/v1/places:searchNearby", json=data, headers=headers)
 
+    weight_array = []
     if response.status_code == 200:
         data = response.json()
         filtered_results = filter_data(data.get('places', []), price, openNow)
         for restaurant in filtered_results:
+            weight = 0
             place_id = restaurant.get("id")
             existing_restaurant = Restaurant.objects.filter(place_id=place_id).first()
+            review_count = len(CustomerReviews.objects.filter(restaurant_id=existing_restaurant).all())
+            if review_count <= 25:
+                weight += .4
+            if review_count <= 50 and review_count > 25:
+                weight += .3
+            if review_count <= 75 and review_count > 50:
+                weight += .2
+            if review_count > 75:
+                weight += .1
+            if existing_restaurant and existing_restaurant.claimed is True:
+                weight += .3
+            if existing_restaurant and existing_restaurant.retaurant_level:
+                weight += existing_restaurant.retaurant_level * .1
+            weight_array.append(weight)
+
             if existing_restaurant:
                 restaurant["id"] = existing_restaurant.id
                 continue
@@ -114,7 +133,20 @@ def query_restaraurant(request):
                 new_restaurant = Restaurant(place_id=place_id, business_name=restaurant.get("displayName", {}).get("text"), claimed=False)
                 new_restaurant.save()
                 restaurant["id"] = new_restaurant.id
-        return JsonResponse({"result": filtered_results}, status=200)
+        random_array_with_weight = []
+        for weight in weight_array:
+            new_weight = weight * random.randint(1,100)
+            random_array_with_weight.append(new_weight)
+      
+        index_of_weights = sorted(range(len(random_array_with_weight)), key=lambda i: random_array_with_weight[i], reverse=True)[:max_result_count]
+      
+
+        results_for_fe = []
+        for index in index_of_weights:
+            results_for_fe.append(filtered_results[index])
+        
+        print(results_for_fe)
+        return JsonResponse({"result": results_for_fe}, status=200)
     else:
         return JsonResponse({"error": "Failed to fetch data from Google Places API"}, status=response.status_code)
 
@@ -154,14 +186,21 @@ def add_to_user_history(request):
     body = request.data
     restaurant_id = body.get('restaurant_id', None)
     user_uid = body.get('uid', None)
-    current_date = timezone.now()
 
     user = User.objects.filter(user_uid=user_uid).first()
     restaurant = Restaurant.objects.filter(id=restaurant_id).first()
     
     if user: 
-        new_history = visited_history(restaurant_id=restaurant, user_id=user, date_visited=current_date)
+        new_history = visited_history(restaurant_id=restaurant, user_id=user)
         new_history.save()
         return HttpResponse("success", status=200)
     else: 
         return JsonResponse({"error": "failed to save history"}, status=500)
+
+
+# @api_view(["POST"])
+# @csrf_exempt
+# def new_review(request):
+#     body = request.data
+    
+    
