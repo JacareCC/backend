ff9c305159ac6e0aa110730897032cb5872c5f72
Kai Eugene <kai.yougene@gmail.com>
2023-12-01 12:35:35 +0900

    feat: restructure user views, urls

---
+++ user/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+class User(models.Model):
+    user_uid = models.CharField(max_length=255, unique=True, blank=False)
+    user_name = models.CharField(max_length=100, null=True)
+    birthday = models.DateField(null=True)
+    level = models.IntegerField(null=True)
+    date_joined = models.DateField(auto_now_add=True)
+
+class UserTier(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant_id = models.ForeignKey('business.Restaurant', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    expires_at = models.DateTimeField(null=True)
+    tier_level = models.ForeignKey('business.TierReward', on_delete=models.CASCADE)
+
+class Points(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    value = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_update = models.DateTimeField(auto_now=True)
+    expires_at = models.DateTimeField(null=True)
+
+class VisitedHistory(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    restaurant_id = models.ForeignKey('business.Restaurant', on_delete=models.CASCADE)
+    date_visited = models.DateTimeField(auto_now_add=True)
+    saved = models.BooleanField(default=False)
