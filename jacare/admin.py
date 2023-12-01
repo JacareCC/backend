6542c3b6f6ea880b2618b82246c8a0b074f4dd4b
Kai Eugene <kai.yougene@gmail.com>
2023-12-01 12:37:27 +0900

    feat: restructure jacare app

--- jacare/admin.py
+++ jacare/admin.py
@@ -1,27 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.models import User
-from jacare.models import RegistrationRequests, Restaurant
 
 
 
-class RegistrationRequestsAdmin(admin.ModelAdmin):
-    list_display = ('user_id', 'business_name', 'status', )
-    actions = ['approve_requests', 'reject_requests']
 
-    readonly_fields = ('user_id', 'first_name', 'last_name', 'business_name', 'email', 'contact_person', 'address', 'phone_number')
 
-    def approve_requests(self, request, queryset):
-        queryset.update(status='approved')
 
-    def reject_requests(self, request, queryset):
-        queryset.update(status='rejected')
-
-admin.site.register(RegistrationRequests, RegistrationRequestsAdmin)
-
-class RestaurantAdmin(admin.ModelAdmin):
-    list_display = ('business_name', 'email', 'phone_number', 'contact_person', 'retaurant_level', 'address', 'claimed', 'owner_user_id')
-    list_filter = ('claimed', 'retaurant_level', 'owner_user_id')
-    search_fields = ('business_name', 'email', 'phone_number', 'contact_person', 'address')
-    list_editable = ('owner_user_id',) 
-
-admin.site.register(Restaurant, RestaurantAdmin)
--- setup/urls.py
+++ setup/urls.py
@@ -1,26 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from jacare.views import login_user, register_user, query_restaraurant, restaurant_detail, user_history, add_to_user_history, new_review, change_user_saved_restaurants, get_user_saved_restaurants, new_registration_request, verify_review, get_reviews, new_tier_level, get_all_tiers, purchase_tier, get_all_user_tiers, edit_tier, delete_tier_level
+from jacare.views import query_restaraurant, restaurant_detail, new_review, purchase_tier
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', login_user),
-    path('register/', register_user),
     path('search/', query_restaraurant),
     path('restaurant/<int:id>/', restaurant_detail),
-    path('user/history/', user_history),
-    path('user/history/add/', add_to_user_history),
-    path('user/tier/all/', get_all_user_tiers),
-    path('submitreview/', new_review),
-    path('user/favorites/', get_user_saved_restaurants),
-    path('user/favorites/add/', change_user_saved_restaurants),
-    path('user/favorites/remove/', change_user_saved_restaurants),
-    path('business/register/', new_registration_request),
-    path('business/review/verify/', verify_review),
-    path('business/reviews/', get_reviews),
-    path('business/tier/new/', new_tier_level),
-    path('business/tier/edit/<int:id>/', edit_tier),
-    path('business/tier/remove/<int:id>/', delete_tier_level),
-    path('tier/all/', get_all_tiers),
+    path('review/new/', new_review),
     path('tier/purchase/', purchase_tier)
 ]
