4f7084d0894a7d795bc4c674475fe7d2c4f729e9
Will Brammer <williamedwardbrammer@gmail.com>
2023-12-05 09:53:20 +0900

    fix: made email in customerreview nullable

--- jacare/models.py
+++ jacare/models.py
@@ -6,7 +6,7 @@ class CustomerReviews(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     restaurant_id = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
     data = models.JSONField(default=dict)
-    email = models.EmailField()
+    email = models.EmailField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     isVerified = models.BooleanField(default=False)
 
