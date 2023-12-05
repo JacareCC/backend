f41015f5382be440e24843f56e61a11d03865a49
Kai Eugene <kai.yougene@gmail.com>
2023-12-06 02:06:44 +0900

    feat: restructure tests

---
+++ jacare/tests/test_login_views.py
@@ -0,0 +1,28 @@
+import pytest
+import json
+import random
+pytestmark = pytest.mark.django_db
+
+class TestLoginEndpoint:
+    def test_successful_login(self, user_factory, api_client):
+        #user object to be used for test data
+        user = user_factory()
+
+        #api call response from /login/ endpoint
+        response = api_client().get("/login/", HTTP_AUTHORIZATION=f'Bearer {user.user_uid}')
+
+        #check if status code and response content are as expected
+        assert response.status_code == 200
+        assert 'login successful' in response.content.decode('utf-8').lower()
+
+    def test_login_with_no_header(self, user_factory, api_client):
+        #user object to be used for test data
+        user = user_factory()
+
+        #api call response from /login/ endpoint
+        response = api_client().get("/login/")
+
+        #check if status code and reponse content are as expected
+        assert response.status_code == 401
+        assert 'authentication required' in response.content.decode('utf-8').lower()
+
