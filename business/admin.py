40d8f8ffd07fa1f06de38e7d65d5227826cd93a3
Kai Eugene <kai.yougene@gmail.com>
2023-12-06 23:51:30 +0900

    feat: qr code contains logo

--- business/admin.py
+++ business/admin.py
@@ -20,7 +20,7 @@ class RegistrationRequestsAdmin(admin.ModelAdmin):
             restaurant = Restaurant.objects.filter(business_name__iexact=business_name).first()
             restaurant.owner_user_id = request_obj.user_id
             restaurant.claimed = True
-            restaurant.qr_code_link = f"https://quickchart.io/qr?text=https%3A%2F%2Fwww.jacareview.com%2Frestaurant%2Frewards%2F{restaurant.id}&dark=080707&size=200g"
+            restaurant.qr_code_link = f"https://quickchart.io/qr?text=https%3A%2F%2Fwww.jacareview.com%2Frestaurant%2Frewards%2F{restaurant.id}&dark=080707&size=200¢erImageUrl=https%3A%2F%2Fwww.jacareview.com%2F_next%2Fimage%3Furl%3D%252F_next%252Fstatic%252Fmedia%252Flogo-home--.135443cd.png%26w%3D1080%26q%3D75"
             restaurant.save()
 
 admin.site.register(RegistrationRequests, RegistrationRequestsAdmin)
