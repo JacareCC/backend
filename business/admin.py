058a5b8656733f18d581d2a9af16b6d404502daf
Kai Eugene <kai.yougene@gmail.com>
2023-12-07 13:06:40 +0900

    fixed: qr code link

--- business/admin.py
+++ business/admin.py
@@ -21,7 +21,7 @@ class RegistrationRequestsAdmin(admin.ModelAdmin):
                 restaurant = Restaurant.objects.get(business_name__iexact=business_name)
                 restaurant.owner_user_id = request_obj.user_id
                 restaurant.claimed = True
-                restaurant.qr_code_link = f"https://quickchart.io/qr?text=http%3A%2F%2Flocalhost%3A3000%2Frestaurants%2Frewards%3Fid%3D{restaurant.id}&size=200¢erImageUrl=https%3A%2F%2Fwww.jacareview.com%2F_next%2Fimage%3Furl%3D%252F_next%252Fstatic%252Fmedia%252Flogo-home--.135443cd.png%26w%3D1080%26q%3D75"
+                restaurant.qr_code_link = f"https://quickchart.io/qr?text=http%3A%2F%2Fjacareview.com%3A3000%2Frestaurants%2Frewards%3Fid%3D{restaurant.id}&size=200¢erImageUrl=https%3A%2F%2Fwww.jacareview.com%2F_next%2Fimage%3Furl%3D%252F_next%252Fstatic%252Fmedia%252Flogo-home--.135443cd.png%26w%3D1080%26q%3D75"
                 restaurant.save()
             except Restaurant.DoesNotExist:
                 print(f"No matching restaurant found for business name: {business_name}")
