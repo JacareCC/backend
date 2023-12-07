7d944120193167d24fa0d6abfed517fc3a461b40
Kai Eugene <kai.yougene@gmail.com>
2023-12-07 09:12:17 +0900

    fix: error handle

--- business/admin.py
+++ business/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from business.models import RegistrationRequests, Restaurant
-from django.core.exceptions import DoesNotExist
 
 # Register your models here.
 class RegistrationRequestsAdmin(admin.ModelAdmin):
@@ -25,7 +24,7 @@ class RegistrationRequestsAdmin(admin.ModelAdmin):
                 restaurant.claimed = True
                 restaurant.qr_code_link = f"https://quickchart.io/qr?text=https%3A%2F%2Fwww.jacareview.com%2Frestaurant%2Frewards%2F{restaurant.id}&dark=080707&size=200¢erImageUrl=https%3A%2F%2Fwww.jacareview.com%2F_next%2Fimage%3Furl%3D%252F_next%252Fstatic%252Fmedia%252Flogo-home--.135443cd.png%26w%3D1080%26q%3D75"
                 restaurant.save()
-            except DoesNotExist:
+            except Restaurant.DoesNotExist:
                 print(f"No matching restaurant found for business name: {business_name}")
             return
             
