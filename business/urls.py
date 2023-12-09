3edea00dec9d38ddbe3ca55d227eab5b767bdd85
Kai Eugene <kai.yougene@gmail.com>
2023-12-09 20:38:48 +0900

    feat: very business user url

--- business/urls.py
+++ business/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-from business.views import new_registration_request, verify_review, get_business, get_specific_business, new_tier_level, edit_tier, delete_tier_level, hide_review
+from business.views import new_registration_request, verify_review, get_business, get_specific_business, new_tier_level, edit_tier, delete_tier_level, hide_review, verify_user
 
 
 
 urlpatterns = [
     path('register/', new_registration_request),
+    path('user/verify/', verify_user),
     path('review/verify/', verify_review),
     path('review/hide/', hide_review),
     path('profile/', get_business),
