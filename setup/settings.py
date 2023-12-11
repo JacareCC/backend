dc14cddaccac98210d223484f2e5c0e6b063b3e3
Will Brammer <williamedwardbrammer@gmail.com>
2023-12-11 19:19:47 +0900

    feat: implement buy on cooldown popup

--- setup/settings.py
+++ setup/settings.py
@@ -91,6 +91,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv("DB_NAME"),
         'USER': os.getenv("DB_USER"),
+         'PASSWORD': os.getenv("DB_PASSWORD"),
         'HOST': 'localhost',
         'PORT': os.getenv("DB_PORT"), 
     }
