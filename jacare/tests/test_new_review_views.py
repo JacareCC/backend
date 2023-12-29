b3a1ae78b516aa7f833f0e0a42d3ebf4b692bff1
Kai Eugene <kai.yougene@gmail.com>
2023-12-29 18:02:43 +0900

    feat: implement bad review data test

--- jacare/tests/test_new_review_views.py
+++ jacare/tests/test_new_review_views.py
@@ -21,3 +21,18 @@ class TestNewReviewsEndpoint:
 
         #check if status code is as expected 
         assert response.status_code == 201
+
+    def test_no_data_review(self, points_factory, restaurant_factory, user_factory, api_client):
+        #setup data needed for test
+        user = user_factory()
+        points = points_factory()
+        restaurant = restaurant_factory()
+
+
+        #resposne from api called to /review/new/
+        response = api_client().post('/review/new/', content_type='application/json', data=None)
+
+        #check if status code is as expected
+        assert response.status_code == 400 
+    
+    
