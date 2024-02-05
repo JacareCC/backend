cac72b5346adc8ab5f8f69fe3a210f212119bbbb
William Brammer <williamedwardbrammer@gmail.com>
2024-02-06 01:14:23 +0900

    feat: changed cors settings to protect the api
    Co-authored-by: kyougene <kai.yougene@gmail.com>

--- setup/settings.py
+++ setup/settings.py
@@ -23,8 +23,8 @@ DEBUG = True
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = ['http://localhost:3000']
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https://www.jacareview.com/', 'https://frontend-dev-ebon.vercel.app/']
 
 
 CSRF_TRUSTED_ORIGINS = [
